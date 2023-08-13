7d388f07a2f68aae6addac0c8267b61e40289b5d
Austin Greer <austin@Austins-Mac-mini.local>
2023-08-13 12:35:19 -0300

    added build tool

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,57 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync } from "fs";
+import { config } from "../utils/tsconfig";
+import { exec } from "child_process";
 
 const program = new Command();
 
-program.version("1.0.0").description("CLI tool for generating config files");
-
-program
-  .command("tsconfig init")
-  .description("Generate a populated tsconfig.json file")
-  .action(generateTsConfig);
+program.command("create <name> [additionalPackages...]").action(create);
 
 program.parse(process.argv);
 
-function generateTsConfig() {
-  console.log("Generating tsconfig.json file");
+async function create(name: string, additionalPackages: string[]) {
+  console.log("Creating app structure");
 
-  const tsConfig = {
-    compilerOptions: {
-      target: "esnext",
-      module: "esnext",
-      lib: ["es6", "es7", "esnext", "dom"],
-      allowJs: true,
-      jsx: "react",
-      removeComments: false,
-      strict: true,
-      noImplicitAny: true,
-      strictNullChecks: true,
-      strictFunctionTypes: true,
-      strictPropertyInitialization: true,
-      noImplicitThis: true,
-      alwaysStrict: true,
-      noUnusedLocals: true,
-      noUnusedParameters: true,
-      noImplicitReturns: true,
-      noFallthroughCasesInSwitch: true,
-      moduleResolution: "node",
-      baseUrl: ".",
-      allowSyntheticDefaultImports: true,
-      esModuleInterop: true,
-      inlineSourceMap: true,
-      inlineSources: true,
-    },
-    include: ["src/**/*"],
+  await execute(`mkdir ${name}`);
+  await execute(
+    `cd ${name} && yarn init -y && yarn add -D typescript @types/node ts-node nodemon tsup`
+  );
+
+  if (additionalPackages.length > 0) {
+    await execute(`cd ${name} && yarn add ${additionalPackages.join(" ")}`);
+  }
+
+  await execute(`cd ${name} && mkdir src`);
+
+  writeFileSync(`./${name}/src/index.ts`, "", "utf-8");
+  writeFileSync(`./${name}/tsconfig.json`, JSON.stringify(config), "utf-8");
+
+  const json = readFileSync(`./${name}/package.json`, "utf-8");
+  const packageJson = JSON.parse(json);
+
+  packageJson.scripts = {
+    start: "nodemon --watch src --exec ts-node src/index.ts",
+    build: "tsup src/index.ts --format cjs --minify",
   };
 
-  writeFileSync("tsconfig.json", JSON.stringify(tsConfig), "utf-8");
+  writeFileSync(
+    `./${name}/package.json`,
+    JSON.stringify(packageJson, null, 2),
+    "utf-8"
+  );
+}
+
+async function execute(command: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log(stdout);
+        resolve();
+      }
+    });
+  });
 }
